abddb682beb6c6704fa4f459f8f4754a7bc30382
AhmedAyachi <aayachi032@gmail.com>
2024-09-22 15:21:37 +0100

    fix: can't open files in some locations

--- src/android/FileExplorer.java
+++ src/android/FileExplorer.java
@@ -154,7 +154,8 @@ public class FileExplorer extends VritraPlugin {
                 intent.setData(uri);
                 final Activity activity=cordova.getActivity();
                 if(intent.resolveActivity(activity.getPackageManager())==null){
-                    Toast.makeText(context,"No app to open file",Toast.LENGTH_SHORT).show();
+                    throw new Exception("No app to open file");
+                    //Toast.makeText(context,"No app to open file",Toast.LENGTH_SHORT).show();
                 }
                 else{
                     intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION|Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
